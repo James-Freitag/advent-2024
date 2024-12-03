0fec1f7adbfa857ea92dd03df6d80fa39b846a0e
James <jamesfreitag05@yahoo.com>
2024-12-02 21:49:41 -0600

    Advent Part 2

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 import { leftList, rightList } from "./numberList.js";
 
-let leftSort = leftList.sort((a, b) => a - b);
-let rightSort = rightList.sort((a, b) => a - b);
+// Part Two ----------------------------------
+
+const similarityScore = (arr1, arr2) => {
+  const result = [];
+
+  for (const num1 of arr1) {
+    let count = 0;
+
+    for (const num2 of arr2) {
+      if (num1 === num2) {
+        count++;
+      }
+    }
+
+    if (count > 0) {
+      result.push(num1 * count);
+    }
+  }
+  return result;
+};
+
+const addScore = () => {
+  const scores = similarityScore(leftList, rightList);
+  let sum = 0;
+  for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  }
+  return sum;
+};
+
+console.log(addScore());
+
+// Part One ----------------------------
+
+//sort lists
+// let leftSort = leftList.sort((a, b) => a - b);
+// let rightSort = rightList.sort((a, b) => a - b);
 
 const difference = (arr1, arr2) => {
   let arrDiff = [];
@@ -15,4 +50,4 @@ const difference = (arr1, arr2) => {
   console.log(sum);
 };
 
-difference(leftList, rightList);
+// difference(leftList, rightList);
